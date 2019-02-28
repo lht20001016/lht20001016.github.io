f424d689ea56cf2e5b1f5b1608161534872837b7
Blueishrobot <47368825+lht20001016@users.noreply.github.com>
2019-02-28 14:28:40 -0600

    HTML/CSS reference
    Added last year's assignemnt for html and css reference to work on index.html

---
+++ Interactive Scene Project/sketch.js
@@ -0,0 +1,17 @@
+// Shooting Stars
+// Kyle Luo
+// February 26, 2019
+//
+// Extra for Experts:
+// - Added Sound
+// - Made Custom Shaped
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  ellipse(50, 50, 50);
+}
