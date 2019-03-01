9d5f8961815c0bac948f173c6033162b98faf4cd
Blueishrobot <47368825+lht20001016@users.noreply.github.com>
2019-03-01 15:19:59 -0600

    added frames to the game

--- scene/sketch.js
+++ scene/sketch.js
@@ -4,13 +4,26 @@
 //
 // Extra for Experts:
 // - Added Sound
-// - Made Custom Shaped
-
+// - Made Custom Shapes
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(220);
+  createGame();
 }
 
 function draw() {
-  background(220);
+  ellipse(100, 100, 100)
+}
+
+function createGame() {
+  strokeWeight(4);
+  stroke(0, 255, 255);
+  noFill();
+  beginShape();
+  vertex(windowWidth / 3, windowHeight / 6);  
+  vertex(windowWidth / 3, windowHeight / 6 * 5);
+  vertex(windowWidth / 3 * 2, windowHeight / 6 * 5);
+  vertex(windowWidth / 3 * 2, windowHeight / 6);
+  endShape();
 }
