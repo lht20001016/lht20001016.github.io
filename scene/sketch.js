5f58e317457c43434bd2175949832f9a65e28851
blueishrobot <kyleluohappy@gmail.com>
2019-03-03 17:41:54 -0600

    first steps and movement donw

--- scene/sketch.js
+++ scene/sketch.js
@@ -1,4 +1,4 @@
-// Shooting Stars
+// Let The Bullets Fly
 // Kyle Luo
 // February 26, 2019
 //
@@ -6,24 +6,45 @@
 // - Added Sound
 // - Made Custom Shapes
 
+let character;
+let charx;
+let chary;
+let vx;
+let vy;
+let dex;
+let dey;
+
+function preload() {
+  character = loadImage("assets/character.PNG");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220);
-  createGame();
+  charx = windowWidth / 2;
+  chary = windowHeight / 2;
+  dex = charx;
+  dey = chary;
+  vx = 0;
+  vy = 0;
 }
 
 function draw() {
-  ellipse(100, 100, 100)
+  background(250);
+  image(character, charx, chary, windowWidth / 16, windowHeight / 8);
+  if (charx !== dex) {
+    vx = (dex - charx) / 60;
+    vy = (dey - chary) / 60;
+  }
+  if (vx <= 0 || charx <= dex - windowWidth / 16) {
+    charx += vx;
+  }
+  if (vy <= 0 || chary <= dey - windowHeight / 8) {
+    chary += vy;
+  }
 }
 
-function createGame() {
-  strokeWeight(4);
-  stroke(0, 255, 255);
-  noFill();
-  beginShape();
-  vertex(windowWidth / 3, windowHeight / 6);  
-  vertex(windowWidth / 3, windowHeight / 6 * 5);
-  vertex(windowWidth / 3 * 2, windowHeight / 6 * 5);
-  vertex(windowWidth / 3 * 2, windowHeight / 6);
-  endShape();
+function mouseClicked() {
+  dex = mouseX;
+  dey = mouseY;
 }
+
